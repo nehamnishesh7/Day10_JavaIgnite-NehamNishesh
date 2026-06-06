d2bf85c971705c07369ae60699330c0dc1f89807
nehamnishesh7 <nisheshneham7@gmail.com>
2026-06-06 11:41:50 +0530

    Update String_Processor.java

--- String_Processor.java
+++ String_Processor.java
@@ -17,3 +17,29 @@ StringIndexOutOfBoundsException
 Example:
 Invalid index handled safely
 */
+import java.util.Scanner;
+
+public class StringProcessor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String text = scanner.nextLine();
+            int charIndex = Integer.parseInt(scanner.nextLine());
+            int startIndex = Integer.parseInt(scanner.nextLine());
+            int endIndex = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Character found: " + text.charAt(charIndex));
+            System.out.println("Substring created: " + text.substring(startIndex, endIndex));
+
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Invalid index handled safely");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format");
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
